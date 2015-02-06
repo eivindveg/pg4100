c14018864cfcec0ad2883b90cb2c9fe8248ff890
eivindveg <eivindveg@hotmail.com>
2015-02-06 18:15:56 +0100

    assignment1: remove unused field

--- pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/controllers/ClientService.java
+++ pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/controllers/ClientService.java
@@ -8,7 +8,6 @@ import javafx.scene.control.ProgressBar;
 import no.westerdals.student.vegeiv13.assignment1.carrental.CarRental;
 import no.westerdals.student.vegeiv13.assignment1.carrental.Client;
 import no.westerdals.student.vegeiv13.assignment1.carrental.ClientState;
-import no.westerdals.student.vegeiv13.assignment1.carrental.cars.RentalCar;
 import no.westerdals.student.vegeiv13.assignment1.carrental.concurrent.ReadyTask;
 import no.westerdals.student.vegeiv13.assignment1.carrental.concurrent.RentingTask;
 import no.westerdals.student.vegeiv13.assignment1.carrental.concurrent.WaitingTask;
@@ -24,7 +23,6 @@ public class ClientService extends Service<ClientState> {
     private ProgressBar progress;
     @FXML
     private Label label;
-    private RentalCar rentalCar;
     private Client client;
 
 
@@ -67,14 +65,6 @@ public class ClientService extends Service<ClientState> {
 
     private WaitingTask createWaitingTask() {
         return new WaitingTask(getClient(), carRental);
-    }
-
-    public RentalCar getRentalCar() {
-        return rentalCar;
-    }
-
-    public void setRentalCar(final RentalCar rentalCar) {
-        this.rentalCar = rentalCar;
     }
 
     public Client getClient() {
