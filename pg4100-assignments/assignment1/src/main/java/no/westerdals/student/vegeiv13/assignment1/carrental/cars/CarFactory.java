c03e16b08945e1e59ae0cec154c6881c74adbb84
eivindveg <eivindveg@hotmail.com>
2015-02-07 02:01:18 +0100

    assignment1: document the car factory

--- pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/cars/CarFactory.java
+++ pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/cars/CarFactory.java
@@ -4,11 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * CarFactory objects are used to generate cars without having to pass arguments to a car's constructor
+ */
 public class CarFactory {
 
     private final String prefix;
     private final int numbers;
 
+    /**
+     * Creates a CarFactory
+     * @param prefix the prefix for the number plates
+     * @param numbersLength the numeric length of number plates
+     */
     public CarFactory(String prefix, int numbersLength) {
         this.numbers = numbersLength;
         if (prefix == null || prefix.length() != 2) {
@@ -20,15 +28,33 @@ public class CarFactory {
         this.prefix = prefix.toUpperCase();
     }
 
+    /**
+     * Creates a single rental car with a random number plate
+     * @return the newly created Rental Car
+     */
     public RentalCar createRentalCar() {
+        String registrationNumber = createNumberPlate();
+        return new RentalCar(registrationNumber);
+    }
+
+    /**
+     * Builds a number plate with this factory's prefix and the assigned number plate length
+     * @return the generated number plate
+     */
+    private String createNumberPlate() {
         final Random random = new Random();
         final StringBuilder plateBuilder = new StringBuilder(getPrefix());
         for (int i = 0; i < numbers; i++) {
             plateBuilder.append(random.nextInt(9));
         }
-        return new RentalCar(plateBuilder.toString());
+        return plateBuilder.toString();
     }
 
+    /**
+     * Creates n rental cars with random number plates
+     * @param n the amount of cars to create
+     * @return a list containing the created cars
+     */
     public List<RentalCar> createRentalCars(int n) {
         final List<RentalCar> cars = new ArrayList<>();
         for (int i = 0; i < n; i++) {
@@ -37,6 +63,10 @@ public class CarFactory {
         return cars;
     }
 
+    /**
+     * Gets this factory's prefix
+     * @return this factory's prefix
+     */
     public String getPrefix() {
         return prefix;
     }
--- pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/cars/CarFactoryException.java
+++ pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/cars/CarFactoryException.java
@@ -1,7 +1,9 @@
 package no.westerdals.student.vegeiv13.assignment1.carrental.cars;
 
+/**
+ * Thrown when a CarFactory is attempted set up improperly
+ */
 public class CarFactoryException extends RuntimeException {
-
     public CarFactoryException(String message) {
         super(message);
     }
