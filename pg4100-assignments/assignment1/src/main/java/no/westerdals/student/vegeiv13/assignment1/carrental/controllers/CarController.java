7dbf358d09123616cfc9208b00ac9b7bb045886d
eivindveg <eivindveg@hotmail.com>
2015-05-05 19:15:14 +0200

    convert expression to lambda

--- pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/controllers/CarController.java
+++ pg4100-assignments/assignment1/src/main/java/no/westerdals/student/vegeiv13/assignment1/carrental/controllers/CarController.java
@@ -1,8 +1,6 @@
 package no.westerdals.student.vegeiv13.assignment1.carrental.controllers;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import no.westerdals.student.vegeiv13.assignment1.carrental.cars.RentalCar;
@@ -45,17 +43,14 @@ public class CarController {
             bindToClient(car.getRentedBy());
         }
 
-        car.clientProperty().addListener(new ChangeListener<Client>() {
-            @Override
-            public void changed(final ObservableValue<? extends Client> observable, final Client oldValue, final Client newValue) {
-                if (newValue != null) {
-                    Platform.runLater(() -> bindToClient(newValue));
-                } else {
-                    Platform.runLater(() -> {
-                        clientLabel.textProperty().unbind();
-                        clientLabel.setText("Available");
-                    });
-                }
+        car.clientProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                Platform.runLater(() -> bindToClient(newValue));
+            } else {
+                Platform.runLater(() -> {
+                    clientLabel.textProperty().unbind();
+                    clientLabel.setText("Available");
+                });
             }
         });
         carLabel.textProperty().bind(car.registrationNumberProperty());
